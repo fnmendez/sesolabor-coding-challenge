da6b96a87be99ef26d5ddc8ba69134e3cf2a26ba
Franco Mendez Z <f@francomendez.com>
2022-10-24 19:49:28 -0300

    feat: add sync sorted merge min heap solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,8 +1,53 @@
 "use strict";
 
+const MinHeap = require("../lib/min-heap");
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  // minHeapSolution(logSources, printer);
+  minHeapSolution(logSources, printer);
   return console.log("Sync sort complete.");
 };
+
+/**
+ * For a data structure approach, the optimal is to use a MinHeap of size n as we know
+ * that the logEntries within a same logSource are in chronological order.
+ * So they can only be unsorted in between the different n logSources.
+ *
+ * O(k*n*log k) time complexity, as there are n*k logEntries to push/poll from minHeap
+ * O(k) space complexity, as we only store k logSources in the minHeap
+ *
+ * k being amount of logSources
+ * n being amount of logEntries per logSource (as stated in README)
+ */
+
+// const USE_TIME_IMPROVEMENT = true; // Did not improve, just for reference
+function minHeapSolution(logSources, printer) {
+  const minHeap = new MinHeap();
+  // First MinHeap fill
+  for (let i = 0; i < logSources.length; i++) {
+    const logEntry = logSources[i].pop();
+    if (logEntry) {
+      minHeap.push(logEntry, i);
+    }
+  }
+  // MinHeap pop and push from the same logSource (that's why we need to push the index)
+  while (minHeap.length() > 0) {
+    const [value, index] = minHeap.poll();
+    printer.print(value);
+    const logEntry = logSources[index].pop();
+    /**
+     * Time improvement *try*:
+     * Check whether the logEntry we are going to push would be polled in the next iteration.
+     * => Did not improve significantly (just for reference).
+     */
+    // while (USE_TIME_IMPROVEMENT && logEntry && logEntry.date < minHeap.peek()[0].date) {
+    //   printer.print(logEntry);
+    //   logEntry = logSources[index].pop();
+    // }
+    if (logEntry) {
+      minHeap.push(logEntry, index);
+    }
+  }
+  printer.done();
+}
